0b4461a5e4d82b5725ef6c76a7cfd7ea8ccd48e7
jalep <jalep@DESKTOP-7RCB1ST>
2020-02-26 22:51:18 +0100

    .

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='muss',
       version='0.1',
       description='Python code execution module',
       author='jalepi',
       author_email='jalepi@live.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/jalepi/muss',
       packages=[]
     )
